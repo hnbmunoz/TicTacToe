effcae2265507913071360f1394260e0dfa12cd0
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2022-10-18 22:13:20 +0800

    Dark-Mode-Updated

--- src/Components/playerSelection.js
+++ src/Components/playerSelection.js
@@ -34,6 +34,9 @@ const playerX = (PlayerSelected, vsMode) => {
   let divX = CreateElement('div');  
   let playerX = CreateElement("button");
   playerX.classList.add('player-selection');
+  if (document.getElementById('darkModeSlider').children.sliderElement.children.sliderSwitch.checked) {
+    playerX.classList.add('dark-announcement');  
+  }
   playerX.innerHTML = "X";
   playerX.addEventListener('click', () => {PlayerSelected({mode: vsMode, player1: 'PlayerX'})})
   divX.appendChild(playerX)
@@ -44,6 +47,9 @@ const playerO = (PlayerSelected, vsMode) => {
   let divO = CreateElement('div');
   let playerO = CreateElement("button");
   playerO.classList.add('player-selection');
+  if (document.getElementById('darkModeSlider').children.sliderElement.children.sliderSwitch.checked) {
+    playerO.classList.add('dark-announcement');  
+  }
   playerO.innerHTML = "O";
   playerO.addEventListener('click', () => {PlayerSelected({mode: vsMode, player1: 'PlayerO'})})
   divO.appendChild(playerO)
